523efde1f6e33ffd720e52868e8972dec734994f
Vadim <opizah70@gmail.com>
2022-12-18 23:47:29 +0500

    /

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,35 @@
+﻿int[,,] CreateMatrix(int row, int col, int dep, int min, int max)
+{
+    int[,,] matrix = new int[row, col, dep];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = rnd.Next(min, max + 1);
+            }
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write($"{matrix[i, j, k],1}|");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,,] array3D = CreateMatrix(2, 2, 2, 10, 99);
+PrintMatrix(array3D);
